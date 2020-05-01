a79b38cb48a83bbc2605e4d3beb4c9204539ad14
Emilian Manolache <e.manolache@dataclaritycorp.com>
2020-05-01 11:43:12 +0300

    Implement ability to exclude skills/tags

--- src/content.js
+++ src/content.js
@@ -4,25 +4,30 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
 });
 
 // apply filters
-chrome.storage.sync.get(['type', 'budget', 'excludedLocations', 'excludedKeywords'], (items) => {
+chrome.storage.sync.get(['type', 'budget', 'excludedLocations', 'excludedKeywords', 'excludedTags'], (items) => {
     setInterval(function() {
+        let targetPageSignature = document.querySelector('span[data-advanced-search]');
+        if (!targetPageSignature) return;
+
         let type = items.type || "All";
         let budget = parseInt(items.budget) || 0;
         let excludedLocations = (items.excludedLocations || "").split(",").map(item => item.trim());
         let excludedKeywords = (items.excludedKeywords || "").split(",").map(item => item.trim());
+        let excludedTags = (items.excludedTags || "").split(",").map(item => item.trim());
 
         let jobs = document.getElementsByClassName("job-tile");
-        if (jobs.length) filterJobs(jobs, type, budget, excludedLocations, excludedKeywords);
+        if (jobs.length) filterJobs(jobs, type, budget, excludedLocations, excludedKeywords, excludedTags);
 
-    }, 200);
+    }, 500);
 });
 
-function filterJobs(jobs, type, budget, excludedLocations, excludedKeywords) {
+function filterJobs(jobs, type, budget, excludedLocations, excludedKeywords, excludedTags) {
     for (let job of jobs) {
         let jobTypeContainer = job.getElementsByClassName('js-type');
         let budgetContainer = job.getElementsByClassName('js-budget');
         let locationContainer = job.getElementsByClassName('client-location');
         let titleContainer = job.getElementsByClassName('job-title');
+        let tagsContainer = job.getElementsByClassName('js-skills');
 
         if (budgetContainer.length) budgetContainer = budgetContainer[0].getElementsByTagName('span');
 
@@ -30,11 +35,17 @@ function filterJobs(jobs, type, budget, excludedLocations, excludedKeywords) {
         let jobBudget = budgetContainer.length ? parseInt(budgetContainer[1].textContent.replace(/\$/gi, '').replace(/,/gi, '')) : null;
         let location = locationContainer.length ? locationContainer[0].textContent : null;
         let title = titleContainer.length ? titleContainer[0].textContent : null;
-        let keywords = title ? title.replace(/-/, ' ').toLowerCase().split(" ").map(item => item.trim()).filter(item => item) : [];
+        let tags = tagsContainer.length ? tagsContainer[0].getElementsByClassName('js-skill') : [];
+        if (tags) tags = Array.from(tags).map(item => item.textContent.toLowerCase().trim());
+
+        if (title) title = title.replace(/-/, ' ').toLowerCase();
+        let keywords = title ? title.split(" ").map(item => item.trim()).filter(item => item) : [];
 
         if (jobType && type !== "All" && jobType !== type) job.remove();
         if (jobBudget && jobBudget < budget) job.remove();
         if (location && excludedLocations && excludedLocations.some(item => item.toLowerCase() === location.toLowerCase())) job.remove();
         if (keywords && keywords.length && excludedKeywords && excludedKeywords.some(item => keywords.indexOf(item.toLowerCase()) !== -1)) job.remove();
+        if (title && excludedKeywords.some(item => item.indexOf(" ") !== -1 && title.indexOf(item.toLowerCase()) !== -1)) job.remove();
+        if (tags && excludedTags.some(item => tags.indexOf(item.toLowerCase()) !== -1)) job.remove();
     }
 }
--- src/popup.js
+++ src/popup.js
@@ -3,16 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let budgetInput = document.getElementById('budget');
     let excludedLocationsInput = document.getElementById('excludedLocations');
     let excludedKeywordsInput = document.getElementById('excludedKeywords');
+    let excludedTagsInput = document.getElementById('excludedTags');
 
-    chrome.storage.sync.get(['type', 'budget', 'excludedLocations', 'excludedKeywords'], (items) => {
+    chrome.storage.sync.get(['type', 'budget', 'excludedLocations', 'excludedKeywords', 'excludedTags'], (items) => {
         let type = items.type || "All";
         let budget = parseInt(items.budget) || 0;
         let excludedLocations = (items.excludedLocations || "").split(",").map(item => item.trim()).join(",");
         let excludedKeywords = (items.excludedKeywords || "").split(",").map(item => item.trim()).join(",");
+        let excludedTags = (items.excludedTags || "").split(",").map(item => item.trim()).join(",");
         typeSelect.value = type;
         budgetInput.value = budget;
         excludedLocationsInput.value = excludedLocations;
         excludedKeywordsInput.value = excludedKeywords;
+        excludedTagsInput.value = excludedTags;
     });
 
     typeSelect.onchange = function (element) {
@@ -33,6 +36,11 @@ document.addEventListener('DOMContentLoaded', function() {
     excludedKeywordsInput.onchange = function (element) {
         chrome.storage.sync.set({
             'excludedKeywords': element.target.value
+        });
+    };
+    excludedTagsInput.onchange = function (element) {
+        chrome.storage.sync.set({
+            'excludedTags': element.target.value
         });
     };
 }, false);
