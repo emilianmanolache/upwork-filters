4e2de5e41e616f9f7b654dd29b63e8e4c92ba77a
Emilian Manolache <e.manolache@dataclaritycorp.com>
2020-05-01 11:17:06 +0300

    Activate extension only on job search page

--- src/background.js
+++ src/background.js
@@ -2,7 +2,10 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostContains: '.upwork.com'},
+				pageUrl: {
+					hostContains: '.upwork.com'
+				},
+				css: ["span[data-advanced-search]"]
 			})
 			],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
